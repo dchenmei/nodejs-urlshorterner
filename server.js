0105d0073c78e6a1d3b3f938f636a0e2ad6f6bff
dchenmei <dchenmei@users.noreply.github.com>
2019-04-17 21:21:06 -0500

    Addes insert url without checking for duplicates

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var dotenv = require('dotenv');
+dotenv.config();
+
 var express = require('express');
 var mongo = require('mongodb');
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
@@ -22,15 +25,43 @@ app.use(bodyParser.urlencoded({ extended : false }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+mongo.connect(process.env.DATABASE, (err, client) => {
+	if (err) {
+		console.log("Oops, looks like the database is broken");
+	}
+	
+	const db = client.db('urls');
 
-app.post("/api/shorturl/new", function (req, res) {
-	res.json({"original_url" : req.body.url, "short_url" : 111});
-});
+	/*
+	db.collection('shortUrls').findOne({url:"www.google.com"}, (err, data) => {
+		if (err) {
+			console.log("Something wrong with the db");
+		} else if (data == null) {
+			console.log("Not found...");
+		} else {
+			console.log(data);
+		}
+	});
+	*/
+
+	app.get('/', function(req, res){
+	  res.sendFile(process.cwd() + '/views/index.html');
+	});
+
+	app.post("/api/shorturl/new", function (req, res) {
+
+		db.collection('shortUrls').insertOne({url: req.body.url}, (err, id) => {
+			if (err) {
+				console.log("Something bad went wrong");
+			} else {
+				res.json({"original_url" : req.body.url, "short_url" : 111});
+			}
+		});
+		
+	});
 
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+	app.listen(port, function () {
+	  console.log('Node.js listening ...');
+	});
 });
